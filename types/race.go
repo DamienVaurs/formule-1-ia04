20f8191628f998e85a76777b3d065501986a02f9
Damien Vaurs <damien.vaurs@etu.utc.fr>
2023-11-13 11:42:15 +0000

    constructeurs

--- types/race.go
+++ types/race.go
@@ -11,3 +11,25 @@ type Race struct {
 	FinalResult    []*Driver   // Final result, drivers rank from 1st to last
 	HighLigths     []Highlight // Containes all what happend during the race
 }
+
+func NewRace(id string, circuit *Circuit, date time.Time, drivers []*Driver, meteo Meteo, finalResult []*Driver, highlights []Highlight) *Race {
+
+	d := make([]*Driver, len(drivers))
+	copy(d, drivers)
+
+	f := make([]*Driver, len(finalResult))
+	copy(f, finalResult)
+
+	h := make([]Highlight, len(highlights))
+	copy(h, highlights)
+
+	return &Race{
+		Id:             id,
+		Circuit:        circuit,
+		Date:           date,
+		Drivers:        d,
+		MeteoCondition: meteo,
+		FinalResult:    f,
+		HighLigths:     h,
+	}
+}
