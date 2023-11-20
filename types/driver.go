6a59fbefd68549afc257fbf958a87d3d9e2e1112
milairhu <hugo.milair@gmail.com>
2023-11-20 12:58:56 +0100

    Ajoute classe DriverInRace

--- types/driver.go
+++ types/driver.go
@@ -14,7 +14,20 @@ type Driver struct {
 
 }
 
+type DriverInRace struct {
+	Driver   *Driver  //Pilote lui même
+	Position *Portion //Position du pilote
+	NbLaps   int      //Nombre de tours effectués
+
+	//Pour l'implémentation:
+	// - On a un channel pour recevoir les messages de l'environnement
+	// - On a un channel pour envoyer les messages à l'environnement
+	ChanEnvIn  chan int
+	ChanEnvOut chan int
+}
+
 func NewDriver(id string, firstname string, lastname string, level int, country string, team *Team, personnality Personnality) *Driver {
+
 	return &Driver{
 		Id:                 id,
 		Firstname:          firstname,
@@ -25,6 +38,34 @@ func NewDriver(id string, firstname string, lastname string, level int, country 
 		Personnality:       personnality,
 		ChampionshipPoints: 0,
 	}
+}
+
+func NewDriverInRace(driver *Driver, position *Portion) *DriverInRace {
+	cout := make(chan int)
+	return &DriverInRace{
+		Driver:     driver,
+		Position:   position,
+		NbLaps:     0,
+		ChanEnvOut: cout,
+	}
+}
+
+func SliceOfDrivers(teams []*Team, portionDepart *Portion) []*DriverInRace {
+	res := make([]*DriverInRace, 0)
+	for _, team := range teams {
+		for _, driver := range team.Drivers {
+			d := NewDriverInRace(&driver, portionDepart)
+			res = append(res, d)
+		}
+	}
+	return res
+}
+
+func ShuffleDrivers(drivers []*DriverInRace) []*DriverInRace {
+	rand.Shuffle(len(drivers), func(i, j int) {
+		drivers[i], drivers[j] = drivers[j], drivers[i]
+	})
+	return drivers
 }
 
 func (d *Driver) Overtake(otherDriver *Driver, portion *Portion) (reussite bool, crashedDrivers []*Driver) {
@@ -70,5 +111,31 @@ func (d *Driver) Overtake(otherDriver *Driver, portion *Portion) (reussite bool,
 
 	// Dans le cas par défaut, le doublement est échoué mais aucun crash n'a lieu
 	return false, []*Driver{}
+
+}
+
+func (d *DriverInRace) Start(canVersRace chan int, canDepuisRace chan int, position *Portion, nbLaps int) {
+
+	//On stocke le chanel entrant
+	d.ChanEnvIn = canDepuisRace
+	d.ChanEnvOut = canVersRace
+
+	for {
+		//On attend que l'environnement nous dise qu'on peut jouer
+		<-d.ChanEnvIn
+
+		//On joue
+		//TOD
+
+		//On vérifie si on a fini la course
+		if d.NbLaps == nbLaps {
+			//On envoie la fin à l'environnement
+			d.ChanEnvOut <- 1
+			return
+		} else {
+			d.ChanEnvOut <- 0
+		}
+
+	}
 
 }
--- types/race.go
+++ types/race.go
@@ -1,15 +1,21 @@
 package types
 
-import "time"
+import (
+	"time"
+)
 
 type Race struct {
 	Id             string      // Race ID
 	Circuit        *Circuit    // Circuit
 	Date           time.Time   // Date
-	Team           []*Team     // Set of teams
+	Teams          []*Team     // Set of teams
 	MeteoCondition Meteo       // Meteo condition
 	FinalResult    []*Driver   // Final result, drivers rank from 1st to last
 	HighLigths     []Highlight // Containes all what happend during the race
+
+	//Pour l'implémentation:
+	CIn  chan int   //Canal pour recevoir les messages des pilotes
+	COut []chan int //Canal pour envoyer les messages aux pilotes (1 par pilote)
 }
 
 func NewRace(id string, circuit *Circuit, date time.Time, teams []*Team, meteo Meteo) *Race {
@@ -17,33 +23,50 @@ func NewRace(id string, circuit *Circuit, date time.Time, teams []*Team, meteo M
 	d := make([]*Team, len(teams))
 	copy(d, teams)
 
-	f := make([]*Driver, len(teams)*2) //car 2 drievrs par team
+	f := make([]*Driver, len(teams)*2) //car 2 drivers par team
 
 	h := make([]Highlight, 0)
+
+	cIn := make(chan int)
+	cOut := make([]chan int, len(teams)*2)
 
 	return &Race{
 		Id:             id,
 		Circuit:        circuit,
 		Date:           date,
-		Team:           d,
+		Teams:          d,
 		MeteoCondition: meteo,
 		FinalResult:    f,
 		HighLigths:     h,
+		CIn:            cIn,
+		COut:           cOut,
 	}
 }
 
-/*
 func (r *Race) SimulateRace() {
+	//On crée les instances des pilotes en course
+
+	var drivers = SliceOfDrivers(r.Teams, &(r.Circuit.Portions[0]))
 	//On lance les agents pilotes
-	for _, team := range r.Team {
-		for _, driver := range team.Drivers {
-			go driver.SimulateDriver(r)
+	for i, driver := range drivers {
+		go driver.Start(r.CIn, r.COut[i], driver.Position, driver.NbLaps)
+	}
+
+	var nbFinish = 0
+	var nbDrivers = len(r.Teams) * 2
+
+	//On simule tant que tous les pilotes n'ont pas fini la course
+	for nbFinish < nbDrivers {
+		//Chaque pilote, dans un ordre aléatoire, réalise les tests sur la proba de dépasser etc...
+		drivers = ShuffleDrivers(drivers)
+		for _, driver := range drivers {
+			//On dit au pilote qu'il peut jouer
+			driver.ChanEnvIn <- 1
+		}
+		//On attend que tous les pilotes aient fini de jouer
+		for i := 0; i < nbDrivers; i++ {
+			ret := <-r.CIn
+			nbFinish += ret // on dit que le pilote envoie 1 s'il a fini (ou crash), 0 sinon
 		}
 	}
-	var nbFinish = 0
-	//On simule tant que tous les pilotes n'ont pas fini la course
-	for nbFinish < len(r.Team)*2 {
-
-	}
 }
-*/
