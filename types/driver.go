c15e2014eec7e56b9298b7e5fb535786195891f0
DamienVaurs <damien.v560@gmail.com>
2023-11-27 12:13:39 +0100

    Corrections Merge

--- types/driver.go
+++ types/driver.go
@@ -34,57 +34,11 @@ func (d *Driver) PortionSuccess(portion *Portion) bool {
 	// Pour le moment on prend en compte le niveau du pilote et la "difficulté" de la portion
 	probaReussite := 80
 	probaReussite += d.Level * 2
-	probaReussite -= portion.CrashProbability * 2
+	probaReussite -= portion.Difficulty * 2
 
 	var dice int = rand.Intn(99) + 1
 
 	return dice <= probaReussite
-}
-
-func (d *Driver) Overtake(otherDriver *Driver, portion *Portion) (reussite bool, crashedDrivers []*Driver) {
-
-	probaDoubler := 75
-
-	bonus := 0
-
-	if d.Level > otherDriver.Level {
-		bonus = 10
-	} else if d.Level < otherDriver.Level {
-		bonus = -10
-	} else {
-		bonus = 0
-	}
-
-	// Pour le moment on prend en compte le niveau des pilotes et la "difficulté" de la portion
-	probaDoubler += bonus
-	probaDoubler -= portion.CrashProbability * 7
-
-	var dice int = rand.Intn(99) + 1
-
-	// Si on est en dessous de probaDoubler, on double
-	if dice < probaDoubler {
-		return true, []*Driver{}
-	}
-
-	// Sinon, on regarde si on crash
-
-	// Ici on a un échec critique, les deux pilotes crashent
-	if dice > 95 {
-		return false, []*Driver{d, otherDriver}
-	}
-
-	// Ici, un seul pilote crash, on tire au sort lequel
-	if dice > 90 {
-		if dice%2 == 0 {
-			return false, []*Driver{d}
-		} else {
-			return false, []*Driver{otherDriver}
-		}
-	}
-
-	// Dans le cas par défaut, le doublement est échoué mais aucun crash n'a lieu
-	return false, []*Driver{}
-
 }
 
 func (d *Driver) PitStop() {
@@ -93,19 +47,6 @@ func (d *Driver) PitStop() {
 	// On attends ensuite de recevoir un message sur le channel comme quoi le pitstop est terminé ?
 	d.IsPitStop = false
 	d.TimeWoPitStop = 0
-}
-
-func NewDriver(id string, firstname string, lastname string, level int, country string, team *Team, personnality Personnality) *Driver {
-	return &Driver{
-		Id:                 id,
-		Firstname:          firstname,
-		Lastname:           lastname,
-		Level:              level,
-		Country:            country,
-		Team:               team,
-		Personnality:       personnality,
-		ChampionshipPoints: 0,
-	}
 }
 
 func (d *Driver) Overtake(otherDriver *Driver, portion *Portion) (reussite bool, crashedDrivers []*Driver) {
