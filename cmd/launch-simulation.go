ad4ad8fdb4bcfc3f650695400432ab84e6ebc812
DamienVaurs <damien.v560@gmail.com>
2023-12-09 20:05:33 +0100

    Début d'un truc pour le serveur REST

--- cmd/launch-simulation.go
+++ cmd/launch-simulation.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"time"
+
 	"gitlab.utc.fr/vaursdam/formule-1-ia04/simulator"
 	"gitlab.utc.fr/vaursdam/formule-1-ia04/types"
 	"gitlab.utc.fr/vaursdam/formule-1-ia04/utils"
@@ -16,6 +20,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	//On crée des pointeurs vers les équipes et les circuits
 	pointTabCircuit := make([]*types.Circuit, len(c))
 	for i, circuit := range c {
@@ -32,5 +37,29 @@ func main() {
 	//On a les équipes et les circuits, on lance la simulation
 	championship := types.NewChampionship("2023", "Championship 1", pointTabCircuit, pointTabTeam)
 	s := simulator.NewSimulator([]types.Championship{*championship})
-	s.LaunchSimulation()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Bienvenue sur le serveur"))
+	})
+
+	mux.HandleFunc("/api/startSimulation", func(w http.ResponseWriter, r *http.Request) {
+		s.LaunchSimulation()
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Simulation démarrée"))
+	})
+
+	server := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	// lancement du serveur
+	log.Println("Listening on", server.Addr)
+	log.Fatal(server.ListenAndServe())
 }
