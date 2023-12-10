fd353ae84369008986d2960cfefbaef0490107e7
Yannis <yannis.brena@gmail.com>
2023-12-10 11:52:04 +0100

    personnality

--- types/personnality.go
+++ types/personnality.go
@@ -1,30 +1,36 @@
 package types
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Personnality struct {
-	TraitsValue map[*Trait]int // Dictionnary of traits
+	TraitsValue map[string]int // Dictionnaire de traits
 }
 
-func NewPersonnality(traitsValue map[*Trait]int) *Personnality {
-
+func NewPersonnality(traitsValue map[string]int) *Personnality {
 	return &Personnality{
 		TraitsValue: traitsValue,
 	}
 }
 
-// Les traits sont à définir au lancement du programme. La personnalité utilisera les différents traits
-type Trait struct {
-	Id   string    // Trait ID
-	Type TraitType // Trait name
-	//Description string // Trait description
+func GenerateTraits() map[string]int {
+	rand.NewSource(time.Now().UnixNano())
+	// Un trait est un entier entre 1 et 5
+	random_agressivity := rand.Intn(5) + 1   // AGRESSIVITY 	(statique -> impacte les proba de tentatives de dépassement)
+	random_confidence := rand.Intn(5) + 1    // CONFIDENCE  	(dynamique -> impacte un peu la proba de tenter et la proba de réussir un dépassement)
+	random_docility := rand.Intn(5) + 1      // DOCILITY    	(dynamique -> impacte la proba d'écouter la stratégie de l'équipe)
+	random_concentration := rand.Intn(5) + 1 // CONCENTRATION 	(statique -> impacte la proba de réussir un dépassement)
+
+	traits := make(map[string]int, 4)
+	aggressivity := random_agressivity
+	confidence := random_confidence
+	docility := random_docility
+	concentration := random_concentration
+	traits["Aggressivity"] = aggressivity
+	traits["Confidence"] = confidence
+	traits["Docility"] = docility
+	traits["Concentration"] = concentration
+	return traits
 }
-
-type TraitType int //Un trait est un entier entre 1 et 5
-
-const (
-	AGRESSIVITY   TraitType = iota //statique -> impacte les proba de tentatives de dépassement
-	CONFIDENCE                     // dynamique -> impatce un peu la proba de tenter et la proba de réussir un dépassement
-	DOCILITY                       // dynamique -> impacte la proba d'écouter la stratégie de l'équipe
-	CONCENTRATION                  //statique -> impacte la proba de réussir un dépassement
-
-	//TODO compléter avec les autres traits
-)
