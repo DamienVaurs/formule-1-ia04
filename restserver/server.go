77bc847bbb3a13d1137e4933217e77bbae8fedf5
Yannis <yannis.brena@gmail.com>
2023-12-17 10:47:55 +0100

    new server

---
+++ restserver/server.go
@@ -0,0 +1,85 @@
+package restserver
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"gitlab.utc.fr/vaursdam/formule-1-ia04/simulator"
+	"gitlab.utc.fr/vaursdam/formule-1-ia04/types"
+)
+
+type RestServer struct {
+	sync.Mutex
+	id              string
+	addr            string
+	pointTabCircuit []*types.Circuit
+	pointTabTeam    []*types.Team
+}
+
+func NewRestServer(addr string, pointTabCircuit []*types.Circuit, pointTabTeam []*types.Team) *RestServer {
+	return &RestServer{id: addr, addr: addr, pointTabCircuit: pointTabCircuit, pointTabTeam: pointTabTeam}
+}
+
+// Test de la méthode
+func (rsa *RestServer) checkMethod(method string, w http.ResponseWriter, r *http.Request) bool {
+	if r.Method != method {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "method %q not allowed", r.Method)
+		return false
+	}
+	return true
+}
+
+func (rsa *RestServer) startSimulation(w http.ResponseWriter, r *http.Request) {
+
+	// vérification de la méthode de la requête
+	if !rsa.checkMethod("POST", w, r) {
+		return
+	}
+
+	championship := types.NewChampionship("2023", "Championship 1", rsa.pointTabCircuit, rsa.pointTabTeam)
+	s := simulator.NewSimulator([]types.Championship{*championship})
+	s.LaunchSimulation()
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Simulation terminée"))
+}
+
+func (rsa *RestServer) start50Simulation(w http.ResponseWriter, r *http.Request) {
+
+	// vérification de la méthode de la requête
+	if !rsa.checkMethod("POST", w, r) {
+		return
+	}
+
+	//On a les équipes et les circuits, on lance la simulation
+	championship := make([]types.Championship, 0)
+	for i := 0; i < 50; i++ {
+		championship = append(championship, *types.NewChampionship("2023", "Championship 1", rsa.pointTabCircuit, rsa.pointTabTeam))
+	}
+	s := simulator.NewSimulator(championship)
+	s.LaunchSimulation()
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Simulation de 50 championnats terminée"))
+}
+
+func (rsa *RestServer) Start() {
+	// création du multiplexer
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/startSimulation", rsa.startSimulation)
+
+	// création du serveur http
+	s := &http.Server{
+		Addr:           rsa.addr,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20}
+
+	// lancement du serveur
+	log.Println("Listening on", rsa.addr)
+	go log.Fatal(s.ListenAndServe())
+
+}
