64339d803dc02ba74e4469e96e55d315e1eca3b3
Yannis <yannis.brena@gmail.com>
2023-12-19 10:47:27 +0100

    merging method GET and PUT personalities

--- restserver/server.go
+++ restserver/server.go
@@ -26,16 +26,6 @@ func NewRestServer(addr string, pointTabCircuit []*types.Circuit, pointTabTeam [
 	return &RestServer{addr: addr, pointTabCircuit: pointTabCircuit, pointTabTeam: pointTabTeam}
 }
 
-// Test de la méthode
-func (rsa *RestServer) checkMethod(method string, w http.ResponseWriter, r *http.Request) bool {
-	if r.Method != method {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "method %q not allowed", r.Method)
-		return false
-	}
-	return true
-}
-
 // Décodage de la requête /personalities/update
 func (*RestServer) decodeUpdatePersonalityRequest(r *http.Request) (req []types.UpdatePersonalityInfo, err error) {
 	buf := new(bytes.Buffer)
@@ -47,7 +37,7 @@ func (*RestServer) decodeUpdatePersonalityRequest(r *http.Request) (req []types.
 func (rsa *RestServer) startSimulation(w http.ResponseWriter, r *http.Request) {
 
 	// vérification de la méthode de la requête
-	if !rsa.checkMethod("POST", w, r) {
+	if r.Method != "POST" {
 		return
 	}
 
@@ -61,7 +51,7 @@ func (rsa *RestServer) startSimulation(w http.ResponseWriter, r *http.Request) {
 // Obtenir le classements des pilotes à la fin d'un championnat
 func (rsa *RestServer) getChampionshipRank(w http.ResponseWriter, r *http.Request) {
 	// vérification de la méthode de la requête
-	if !rsa.checkMethod("GET", w, r) {
+	if r.Method != "GET" {
 		return
 	}
 
@@ -70,89 +60,84 @@ func (rsa *RestServer) getChampionshipRank(w http.ResponseWriter, r *http.Reques
 }
 
 // Obtenir les personnalités d'une simulation
-func (rsa *RestServer) getPersonnalities(w http.ResponseWriter, r *http.Request) {
-	// vérification de la méthode de la requête
-	if !rsa.checkMethod("GET", w, r) {
-		return
-	}
+func (rsa *RestServer) getAndUpdatePersonnalities(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" { // Obtenir les personnalités
+		driversInfosPersonnalities := make([]types.PersonalityInfo, 0)
 
-	driversInfosPersonnalities := make([]types.PersonalityInfo, 0)
-
-	for _, team := range rsa.pointTabTeam {
-		team := *team
-		for _, driver := range team.Drivers {
-			driverInfo := types.PersonalityInfo{
-				IdDriver:    driver.Id,
-				Lastname:    driver.Lastname,
-				Personality: driver.Personality.TraitsValue,
+		for _, team := range rsa.pointTabTeam {
+			team := *team
+			for _, driver := range team.Drivers {
+				driverInfo := types.PersonalityInfo{
+					IdDriver:    driver.Id,
+					Lastname:    driver.Lastname,
+					Personality: driver.Personality.TraitsValue,
+				}
+				driversInfosPersonnalities = append(driversInfosPersonnalities, driverInfo)
 			}
-			driversInfosPersonnalities = append(driversInfosPersonnalities, driverInfo)
+
 		}
 
-	}
-
-	serial, _ := json.Marshal(driversInfosPersonnalities)
-	w.WriteHeader(http.StatusOK)
-	w.Write(serial)
-}
-
-// Mettre à jour les personnalités
-func (rsa *RestServer) updatePersonalities(w http.ResponseWriter, r *http.Request) {
-	// vérification de la méthode de la requête
-	if !rsa.checkMethod("PUT", w, r) {
+		serial, _ := json.Marshal(driversInfosPersonnalities)
+		w.WriteHeader(http.StatusOK)
+		w.Write(serial)
 		return
-	}
+	} else if r.Method == "PUT" { // Mettre à jour les personnalités
+		// décodage de la requête
+		req, err := rsa.decodeUpdatePersonalityRequest(r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, err.Error())
+			return
+		}
 
-	// décodage de la requête
-	req, err := rsa.decodeUpdatePersonalityRequest(r)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err.Error())
-		return
-	}
+		// Réponse à renvoyer
+		var resp []types.UpdatePersonalityInfo
 
-	// Réponse à renvoyer
-	var resp []types.UpdatePersonalityInfo
-
-	// Parcours des équipes et des pilotes
-	for _, team := range rsa.pointTabTeam {
-		for i := 0; i < 2; i++ {
-			for _, updateReq := range req {
-				if updateReq.IdDriver == team.Drivers[i].Id {
-					// Test des valeurs des différentes personnalités
-					if updateReq.Personality["Aggressivity"] > 5 || updateReq.Personality["Aggressivity"] < 1 ||
-						updateReq.Personality["Confidence"] > 5 || updateReq.Personality["Confidence"] < 1 ||
-						updateReq.Personality["Docility"] > 5 || updateReq.Personality["Docility"] < 1 ||
-						updateReq.Personality["Concentration"] > 5 || updateReq.Personality["Concentration"] < 1 {
-						msg := "Une des valeurs des personnalités entrée est supérieur à 5 ou inférieur à 1"
-						w.WriteHeader(http.StatusBadRequest)
-						serial, _ := json.Marshal(msg)
-						w.Write(serial)
-						return
-					} else {
-						// Mise à jour des valeurs de personnalité du pilote
-						team.Drivers[i].Personality.TraitsValue = map[string]int{
-							"Aggressivity":  updateReq.Personality["Aggressivity"],
-							"Confidence":    updateReq.Personality["Confidence"],
-							"Docility":      updateReq.Personality["Docility"],
-							"Concentration": updateReq.Personality["Concentration"],
+		// Parcours des équipes et des pilotes
+		for _, team := range rsa.pointTabTeam {
+			for i := 0; i < 2; i++ {
+				for _, updateReq := range req {
+					if updateReq.IdDriver == team.Drivers[i].Id {
+						// Test des valeurs des différentes personnalités
+						if updateReq.Personality["Aggressivity"] > 5 || updateReq.Personality["Aggressivity"] < 1 ||
+							updateReq.Personality["Confidence"] > 5 || updateReq.Personality["Confidence"] < 1 ||
+							updateReq.Personality["Docility"] > 5 || updateReq.Personality["Docility"] < 1 ||
+							updateReq.Personality["Concentration"] > 5 || updateReq.Personality["Concentration"] < 1 {
+							msg := "Une des valeurs des personnalités entrée est supérieur à 5 ou inférieur à 1"
+							w.WriteHeader(http.StatusBadRequest)
+							serial, _ := json.Marshal(msg)
+							w.Write(serial)
+							return
+						} else {
+							// Mise à jour des valeurs de personnalité du pilote
+							team.Drivers[i].Personality.TraitsValue = map[string]int{
+								"Aggressivity":  updateReq.Personality["Aggressivity"],
+								"Confidence":    updateReq.Personality["Confidence"],
+								"Docility":      updateReq.Personality["Docility"],
+								"Concentration": updateReq.Personality["Concentration"],
+							}
 						}
-					}
 
-					// Remplissage de la réponse
-					resp = append(resp, types.UpdatePersonalityInfo{
-						IdDriver:    team.Drivers[i].Id,
-						Personality: team.Drivers[i].Personality.TraitsValue,
-					})
+						// Remplissage de la réponse
+						resp = append(resp, types.UpdatePersonalityInfo{
+							IdDriver:    team.Drivers[i].Id,
+							Personality: team.Drivers[i].Personality.TraitsValue,
+						})
+					}
 				}
 			}
 		}
+
+		serial, _ := json.Marshal(resp)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(serial)
+		return
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "method %q not allowed", r.Method)
+		return
 	}
-
-	serial, _ := json.Marshal(resp)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(serial)
 }
 
 func corsMiddleware(next http.Handler) http.Handler {
@@ -175,8 +160,7 @@ func (rsa *RestServer) Start() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/startSimulation", rsa.startSimulation)
 	mux.HandleFunc("/api/driversChampionshipRank", rsa.getChampionshipRank)
-	mux.HandleFunc("/personalities", rsa.getPersonnalities)
-	mux.HandleFunc("/personalities/update", rsa.updatePersonalities)
+	mux.HandleFunc("/personalities", rsa.getAndUpdatePersonnalities)
 
 	corsHandler := corsMiddleware(mux)
 
